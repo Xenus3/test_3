34e9f07081a1ee1bea7b5889f8ed948a0831abbf
KARIM ABERBACH <99872919+Xenus3@users.noreply.github.com>
2023-06-15 11:47:14 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,16 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
 public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        logger.info("Bonjour");
+        logger.warn("ton code tourne");
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
